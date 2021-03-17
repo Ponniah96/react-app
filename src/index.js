452661812a2bd53384e4b397d97f0c1688cb8c58
unknown <ponniahk78@gmail.com>
2021-03-17 21:16:10 +0530

    installghpages

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <p >test</p>
+    <p >tests</p>
   </React.StrictMode>,
   document.getElementById('root')
 );
